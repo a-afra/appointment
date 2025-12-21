c79dc83861ed6019c5710f15a78206e929c34cef
Alireza Afra <a.afra@xaas.ir>
2025-12-21 15:03:12 +0330

    Add custom user manager.

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,24 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
-from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import UserManager
 
 
 class User(AbstractUser):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    uuid = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, db_index=True
+    )
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+    objects = UserManager
+
     def __str__(self):
         return self.get_username()
-    
+
     def save(self, *args, **kwargs):
         if self.email:
             self.username = self.email
